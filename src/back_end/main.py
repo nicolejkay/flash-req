7f91e6f44d4fd1dfd7497687133676b9a90eb2af
Nicole Kay <nicolejkay@gmail.com>
2023-05-14 19:25:14 +1000

    flask server setup

--- src/back_end/main.py
+++ src/back_end/main.py
@@ -1,13 +1,10 @@
-import os
 from flask import Flask
-template_dir = os.path.abspath('../../frontend/src/templates')
-app = Flask(__name__, template_folder=template_dir)
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template('index.html')
-
+    return "Hello, World!"
+    
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=8080)
