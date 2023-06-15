2f684678fc35ca68b1a51e447c69493d6974ac08
Nicole Kay <nicolejkay@gmail.com>
2023-06-15 17:38:59 +0700

    Pretty much working (y)

--- src/api/form.js
+++ src/api/form.js
@@ -1,8 +1,14 @@
 import { Configuration, OpenAIApi } from 'openai';
 
-function get_prompt(productName, productDescription, numFunct, numNonfunct) {
+const role = `you are an experienced requirements engineer working to the best practice for writing system requirements
+            as stated in the INCOSE Systems Engineering Book of Knowledge (SEBoK).
+            You are tasked with writing system requirement documents based on system information that you will be provided.
+            The requirements you write should be in simple English, be unique, verifiable,
+            and provide a starting point for further system development. Use markup language and format the requirements into a table with the following columns: Requirement ID, Requirement Type, Requirement Text. `
+
+function get_prompt(productName, productDescription, numFunct, numNonFunct) {
     return `Below, I will give you some information about a new product and I need you to respond with a set of ${numFunct} functional
-            requirements and ${numNonfunct} non-functional system level requirements that could be used as a starting point for the development
+            requirements and ${numNonFunct} non-functional system level requirements that could be used as a starting point for the development
             of that product.
 
             The product name is ${productName}.
@@ -16,7 +22,7 @@ async function ask_ai(prompt) {
     const completion = await openai.createChatCompletion({
         model: "gpt-3.5-turbo",
         messages: [
-            {"role": "system", "content": "you are an experienced requirements engineer working to the best practice stated in the INCOSE Systems Engineering Book of Knowledge (SEBoK)"},
+            {"role": "system", "content": role},
             {"role": "user", "content": prompt},
         ],
         temperature: 0.7,
@@ -34,7 +40,7 @@ export default async function (request, response) {
     const body = request.body;
     console.log('Request:', body);
 
-    const prompt = get_prompt(body.productName, body.productDescription, body.numFunct, body.numNonfunct);
+    const prompt = get_prompt(body.productName, body.productDescription, body.numFunct, body.numNonFunct);
     console.log('Prompt:', prompt);
 
     const ai = await ask_ai(prompt);
@@ -42,6 +48,6 @@ export default async function (request, response) {
 
     const output = ai;
 
-    return response.send(output);
+    return response.send(`<pre>${output}</pre>`);
 }
 
